d750a39a2b9812bcf0ac9a43fd220982f515fbc1
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-15 15:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ function firstNonRepeatedChar(str) {
 	let obj = {};
 	for (let i = 0; i < str.length; i++){
 		if(obj[str[i]] !== undefined){
-			obj[str[i]] += 1;
-			 else{
-			    obj[str[i]] =1;
-			 }
-		}
+			obj[str[i]] += 1; 
+		}	
+		else{  
+			    obj[str[i]] = 1;
+		 }
+	}
+	
 	for(let  i = 0; i < str.length; i++ ){
 		if(obj[str[i]] == 1){
 			return srt[i];
